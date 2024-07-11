c842eb5311eaa1617b73ded7b31c720ffcf8e91e
Gokulakrishnan0610 <gokulakilan06@gmail.com>
2024-07-11 11:08:22 +0530

    Updated

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,7 +12,7 @@ function Landing() {
                 <Header name={data.state.user} />
                 <div className="flex justify-between gap-7 my-5 flex-wrap">
                     <Card color={"#8272DA"} title={"23"} subtitle={"Chennai"} />
-                    <Card color={"#FD6663"} title={"Date"} subtitle={"Time"} />
+                    <Card color={"#FD6663"} title={"Date"} subtitle={"10:00"} />
                     <Card color={"#FCA201"} title={"Built Using"} subtitle={"React"} />
                 </div>
                 <Todolistcontainer />
